085b2fe3bd3a13b569295ff401d569a9131642c8
forzaster <forzaster@gmail.com>
2022-07-21 23:23:11 +0900

    Modified decorator implementation

--- app/views/pages/test1/main.py
+++ app/views/pages/test1/main.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass
 
 from models.leaderboard import LeaderboardModel
-from views.page import PageBase
-from views.utils import decorator
+from ...page import PageBase
+from ...utils.decorator import rounded
 from .. import tab_leaderboard
 from . import tab_data
 from . import tab_description
@@ -33,10 +33,11 @@ class Test1(PageBase):
     def get_tab_num(self):
         return len(self._tab_info)
 
+    @rounded
     def get_tab_view(self, idx):
         idx = int(idx)
         args = self._tab_info[idx]['args']
-        return decorator.rounded(self._tab_info[idx]['create'](**args))
+        return self._tab_info[idx]['create'](**args)
 
     def get_tab_title(self, idx):
         return self._tab_info[idx]['title']
